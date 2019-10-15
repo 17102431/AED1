6f3ef2082ce39730fd829a8c3b385591f3431e54
saraiva <felipesaraivadossantos@gmail.com>
2019-10-15 10:37:16 -0300

    teste 1

---
+++ git.c
@@ -0,0 +1,11 @@
+https://github.com/17102431/AED1
+
+git add <nome_do_arquivo>
+
+git commit –m “Adicionar qualquer mensagem sobre o commit aqui”
+
+git push origin master
+
+git remote add origin <servidor>
+
+
